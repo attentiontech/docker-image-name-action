da1c6bf5cf50a61b1724b1db09accea9aa47d59e
bithavoc <im@bithavoc.io>
2024-03-20 20:13:23 +0000

    generate name

--- src/imageName.js
+++ src/imageName.js
@@ -1,15 +1,18 @@
+import github from '@actions/github'
+const context = github.context
+
 /**
  * Generates a docker image
  * @param {string} baseName
  * @returns {string} The generated docker image name
  */
 function generateDockerImageName(baseName) {
-  const buildtime = new Date().toTimeString().replace(/:/g, '-')
-  const tag =
-    `${process.env.GITHUB_HEAD_REF}-time-${buildtime}-commit-${process.env.GITHUB_SHA}`.replace(
-      /\//g,
-      '-'
-    )
+  // generate string from the current time in RFC3339 format without special characters
+  const buildtime = new Date().toISOString().replace(/-:/g, '-')
+  const tag = `${context.ref}-time-${buildtime}-commit-${context.sha}`.replace(
+    /\//g,
+    '-'
+  )
   const imageName = `${baseName}:${tag}`
   return imageName
 }
